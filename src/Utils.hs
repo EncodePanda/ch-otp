1cfec6e84ac28df8f0aa20025da7ae67ce46ae92
Pawel Szulc <paul.szulc@gmail.com>
2018-01-14 22:17:44 +0100

    Extend Utils with sendTo function
    Function that allows sending single message to multiple recipients

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,6 +1,12 @@
-module Utils (logInfo) where 
+module Utils (logInfo, sendTo) where
 
-import Control.Distributed.Process
+import           Control.Distributed.Process
+import           Control.Monad               (forM)
+import           Data.Binary
+import           Data.Typeable
 
 logInfo :: [Char] -> Process ()
 logInfo msg = liftIO . putStrLn $ msg
+
+sendTo :: (Binary a, Typeable a) => [ProcessId] -> a -> Process [()]
+sendTo bsPids protocol = forM bsPids $ \pid -> send pid protocol

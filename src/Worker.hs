3d7a7d8f0f36d008c3f52ba249ec4bac3517f163
Pawel Szulc <paul.szulc@gmail.com>
2018-01-14 18:46:38 +0100

    Add initial sketch of workUntilStopped

--- src/Worker.hs
+++ src/Worker.hs
@@ -3,7 +3,9 @@ module Worker(run) where
 import Protocol
 import           Control.Distributed.Process
 
--- stored events with both logical timestamp and process id for total ordering
+{-|
+Stored events with both logical timestamp and process id for total ordering
+-}
 data StoredEvent = StoredEvent Event Timestamp ProcessId
 
 run :: Process ()
@@ -13,5 +15,21 @@ run = do
   events                      <- workUntilStopped workerPid others []
   send masterPid Done
 
+{-|
+Receives messages from external process and from internal generator then
+stores them with timestamp and process id. Handles logical clock ticks.
+-}
 workUntilStopped :: ProcessId -> [ProcessId] -> [StoredEvent] -> Process [StoredEvent]
-workUntilStopped workerPid others events = undefined -- TODO 
+workUntilStopped workerPid others events = do
+  generatorId <- spawnLocal (generator workerPid)
+  cmd         <- expect :: Process Protocol
+  handle cmd generatorId
+
+  where
+    handle :: Protocol -> ProcessId -> Process [StoredEvent]
+    handle protocol generatorId = undefined
+
+generator :: ProcessId -> Process ()
+generator workerPid = return ()
+
+
